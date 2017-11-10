ceb894012ed189b008eea25e62f28f2063f20760
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2017-11-10 13:05:46 +0100

    Moved VcfField

---
+++ src/main/scala/nl/biopet/utils/ngs/vcf/VcfField.scala
@@ -0,0 +1,25 @@
+package nl.biopet.utils.ngs.vcf
+
+import htsjdk.variant.vcf.VCFHeader
+
+case class VcfField(key: String, method: FieldMethod.Value) {
+  def newInfoCount(header: VCFHeader): InfoFieldCounts = {
+    new InfoFieldCounts(header.getInfoHeaderLine(key), method)
+  }
+
+  def newGenotypeCount(header: VCFHeader): GenotypeFieldCounts = {
+    new GenotypeFieldCounts(header, header.getFormatHeaderLine(key), method)
+  }
+
+  override def toString = s"${key}_$method"
+}
+
+object VcfField {
+  def fromArg(arg: String): VcfField = {
+    val values = arg.split(":")
+    require(values.size == 2, s"A field should be formated like: <tag>:<method>. Possible methods: ${FieldMethod.values}")
+    val key: String = values.head
+    val method: FieldMethod.Value = FieldMethod.withName(values(1))
+    VcfField(key, method)
+  }
+}
