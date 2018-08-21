04cb324488b3e2bb402422455d34797ade09dd54
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2018-08-21 14:54:18 +0200

    add test

--- src/test/scala/nl/biopet/utils/ngs/fastq/ValidationTest.scala
+++ src/test/scala/nl/biopet/utils/ngs/fastq/ValidationTest.scala
@@ -2,13 +2,24 @@ package nl.biopet.utils.ngs.fastq
 
 import htsjdk.samtools.fastq.FastqReader
 import nl.biopet.test.BiopetTest
+import org.testng.annotations.Test
+
 import scala.collection.JavaConversions._
 class ValidationTest extends BiopetTest {
 
+  @Test
   def testValidPairs(): Unit = {
     val r1 = new FastqReader(resourceFile("/R1.fq"))
     val r2 = new FastqReader(resourceFile("/R2.fq"))
     val pairs = r1.iterator().toList.zip(r2.iterator().toList)
     pairs.foreach {case (r1, r2) => Validation.checkMate(r1, r2) shouldBe true}
+  }
+
+  @Test
+  def testInvalidPairs(): Unit = {
+    val r1 = new FastqReader(resourceFile("/R1.fq"))
+    val r2 = new FastqReader(resourceFile("/R2_invalid.fq"))
+    val pairs = r1.iterator().toList.zip(r2.iterator().toList)
+    pairs.foreach {case (r1, r2) => Validation.checkMate(r1, r2) shouldBe false}
   }
 }
